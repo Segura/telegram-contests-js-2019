3581fb7dbf4519cfc2bfc83bf4fba4fd61d52a22
clNone <none.zma@gmail.com>
2019-03-12 19:49:07 +0300

    Добавлена отрисовка контролов

---
+++ src/controls.js
@@ -0,0 +1,21 @@
+export const drawControls = (element, controlConfig = []) => {
+    const container = createContainer();
+    controlConfig.forEach((config) => {
+        container.appendChild(createControl(config));
+    });
+    element.appendChild(container);
+};
+
+const createContainer = () => {
+    let container = document.createElement('div');
+    container.className = 'control-container';
+    return container;
+};
+
+const createControl = (config = {}) => {
+    let control = document.createElement('label');
+    control.className = 'seria-toggler';
+    control.innerHTML = `<input type="checkbox" name="${config.name}"/><span class="seria-toggler-checkmark" style="color: ${config.color}"></span>${config.title}`;
+    control.onchange = config.onSeriaToggle;
+    return control;
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
+import { drawControls } from './controls.js'
+
 export const draw = (container, loadDataPromise, config = {}) => {
-    loadDataPromise.then((data) => {
-        console.log(data)
+    loadDataPromise.then((rawData) => {
+        console.log(rawData)
+        const normalizedData = normalizeChartData(rawData);
+        console.log(normalizedData);
+        drawControls(container, normalizedData);
     })
 }
+
+const normalizeChartData = (rawData = {}) => {
+    const names = Object.keys(rawData.names);
+    return names.reduce((result, name) => {
+        result.push({
+            name,
+            color: rawData.colors[name],
+            title: rawData.names[name],
+            data: rawData.columns
+                            .find((column) => column.some((value) => value === name))
+                            .filter((value) => value !== name),
+            onSeriaToggle: handleOnSeriaToggle(name)
+        });
+        return result;
+    }, []);
+};
+
+const handleOnSeriaToggle = (name) => (e) => {
+    if (e) {
+        console.log(`${name}=${e.target.checked}`);
+    }
+};
