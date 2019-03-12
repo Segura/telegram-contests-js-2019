38b8a42b0903123316e5ea01e63bb71d82cd2ce6
clNone <none.zma@gmail.com>
2019-03-12 22:04:51 +0300

    Data normalization performance increased.

--- src/index.js
+++ src/index.js
@@ -5,24 +5,22 @@ export const draw = (container, loadDataPromise, config = {}) => {
         console.log(rawData)
         const normalizedData = normalizeChartData(rawData);
         console.log(normalizedData);
-        drawControls(container, normalizedData);
+        drawControls(container, normalizedData.yAxis);
     })
 }
 
 const normalizeChartData = (rawData = {}) => {
     const names = Object.keys(rawData.names);
-    return names.reduce((result, name) => {
-        result.push({
-            name,
-            color: rawData.colors[name],
-            title: rawData.names[name],
-            data: rawData.columns
-                            .find((column) => column.some((value) => value === name))
-                            .filter((value) => value !== name),
-            onSeriaToggle: handleOnSeriaToggle(name)
-        });
-        return result;
-    }, []);
+    return {
+            xAxis: rawData.columns.find((column) => names.indexOf(column[0]) === -1),
+            yAxis: names.map((name) => ({
+                name,
+                color: rawData.colors[name],
+                title: rawData.names[name],
+                data: rawData.columns.find((column) => column[0] === name),
+                onSeriaToggle: handleOnSeriaToggle(name)
+            }))
+    }
 };
 
 const handleOnSeriaToggle = (name) => (e) => {
