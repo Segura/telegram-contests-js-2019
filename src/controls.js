e8b22675a4b7e41c7060871bfe4a9a0ac239332b
clNone <none.zma@gmail.com>
2019-03-15 19:02:03 +0300

    added scope selector

--- src/controls.js
+++ src/controls.js
@@ -1,23 +1,50 @@
 import './controls.css';
 
-export const drawControls = (element, controlConfig = []) => {
-    const container = createContainer();
-    controlConfig.forEach((config) => {
-        container.appendChild(createControl(config));
-    });
-    element.appendChild(container);
-};
+export class GraphControls {
 
-const createContainer = () => {
-    let container = document.createElement('div');
-    container.className = 'control-container';
-    return container;
-};
+    static createContainer() {
+        let container = document.createElement('div');
+        container.className = 'control-container';
+        return container;
+    }
 
-const createControl = (config = {}) => {
-    let control = document.createElement('label');
-    control.className = 'seria-toggler';
-    control.innerHTML = `<input type="checkbox" checked name="${config.name}"/><span class="seria-toggler-checkmark" style="color: ${config.color}"></span>${config.title}`;
-    control.onchange = config.onSeriaToggle;
-    return control;
-};
+    constructor(container, config = {data: []}) {
+        this.container = container;
+        this.config = config;
+        this.draw = this.draw.bind(this);
+        this.createControl = this.createControl.bind(this);
+    }
+
+    draw() {
+        const container = GraphControls.createContainer();
+        this.config.data.forEach((config) => {
+            container.appendChild(this.createControl(config));
+        });
+        this.container.appendChild(container);
+    }
+
+    handleOnToggle(name) {
+        return (e) => {
+            if (e) {
+                const event = new CustomEvent('toggleSeria', {
+                    detail: {
+                        name,
+                        value: e.target.checked
+                    },
+                    bubbles: true,
+                    cancelable: true
+                });
+
+                this.container.dispatchEvent(event);
+            }
+        }
+    }
+
+    createControl(config = {}) {
+        let control = document.createElement('label');
+        control.className = 'seria-toggler';
+        control.innerHTML = `<input type="checkbox" checked name="${config.name}"/><span class="seria-toggler-checkmark" style="color: ${config.color}"></span>${config.title}`;
+        control.onchange = this.handleOnToggle(config.name);
+        return control;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import { Chart } from './chart'
-import { drawControls } from './controls.js'
+import { GraphControls } from './controls.js'
 import { GraphSelector } from './selector.js'
 
 export const draw = (container, loadDataPromise, config = {}) => {
     loadDataPromise.then((rawData) => {
-        console.log(rawData)
+        console.log(rawData);
         const normalizedData = normalizeChartData(container, rawData);
         console.log(normalizedData);
-        const canvas = createCanvas();
-        container.appendChild(canvas);
-        new Chart(container, normalizedData, config)
-        drawControls(container, normalizedData.yAxis);
-        const graphSelector = new GraphSelector(canvas, normalizedData);
+        new Chart(container, normalizedData, config);
+        const graphSelector = new GraphSelector(container, normalizedData);
         subscribeToEvent(container, 'toggleSeria', graphSelector.toggle);
         graphSelector.draw();
+        const graphControls = new GraphControls(container, {data: normalizedData.yAxis});
+        graphControls.draw();
     })
-}
+};
 
 const normalizeChartData = (container, rawData = {}) => {
     const names = Object.keys(rawData.names);
@@ -25,35 +24,10 @@ const normalizeChartData = (container, rawData = {}) => {
                 name,
                 color: rawData.colors[name],
                 title: rawData.names[name],
-                data: rawData.columns.find((column) => column[0] === name),
-                onSeriaToggle: handleOnSeriaToggle(container, name)
+                data: rawData.columns.find((column) => column[0] === name)
             }))
     }
 };
-
-const handleOnSeriaToggle = (container, name) => (e) => {
-    if (e) {
-        const event = new CustomEvent('toggleSeria', { 
-            detail: {
-                name,
-                value: e.target.checked
-            },
-            bubbles: true,
-            cancelable: true
-        });
-
-        container.dispatchEvent(event);
-
-        console.log(`${name}=${e.target.checked}`);
-    }
-};
-
-const createCanvas = () => {
-    let canvas = document.createElement('canvas');
-    canvas.className = 'control-canvas';
-    return canvas;
-};
-
 
 const subscribeToEvent = (el, eventName, callback) => {
     el.addEventListener(eventName, (e) => callback(e.detail.name, e.detail.value), false);
