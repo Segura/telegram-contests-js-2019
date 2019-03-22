e9393c73f7f599b5007850044ca55518bb9463de
Зарубин Михаил Александрович <zarubin.m.al@sberbank.ru>
2019-03-22 17:11:54 +0300

    added controls disabled state.

--- src/controls/controls.js
+++ src/controls/controls.js
@@ -1,49 +1,55 @@
-import './controls.css';
+import { EventAware } from '../common/event-aware'
 
-export class GraphControls {
+import './controls.css'
+
+export class GraphControls extends EventAware {
 
     static createContainer() {
-        let container = document.createElement('div');
-        container.className = 'control-container';
-        return container;
+        let container = document.createElement('div')
+        container.className = 'control-container'
+        return container
     }
 
-    constructor(container, config = {data: []}) {
-        this.container = container;
-        this.config = config;
-        this.draw = this.draw.bind(this);
-        this.createControl = this.createControl.bind(this);
+    constructor(container, config = { data: [] }) {
+        super(container)
+        this.config = config
+        this.activeCount = 0
+        this.createControl = this.createControl.bind(this)
         this.draw()
     }
 
     draw() {
-        const container = GraphControls.createContainer();
+        const container = GraphControls.createContainer()
         this.config.data.forEach((config) => {
-            container.appendChild(this.createControl(config));
-        });
-        this.container.appendChild(container);
+            container.appendChild(this.createControl(config))
+        })
+        this.container.appendChild(container)
     }
 
     handleOnToggle(name) {
         return (e) => {
-            const event = new CustomEvent('seriaToggle', {
-                detail: {
-                    name,
-                    value: e.target.checked
-                },
-                bubbles: true,
-                cancelable: true
-            });
-
-            this.container.dispatchEvent(event);
+            if (this.activeCount === 1 && !e.target.checked) {
+                e.preventDefault()
+                e.stopPropagation()
+                e.target.checked = true
+                return
+            }
+            this.activeCount += e.target.checked ? 1 : -1
+            if (this.activeCount === 1) {
+                this.container.classList.add('control-container-disabled')
+            } else {
+                this.container.classList.remove('control-container-disabled')
+            }
+            this.notify('seriaToggle', { name, value: e.target.checked })
         }
     }
 
     createControl(config = {}) {
-        let control = document.createElement('label');
-        control.className = 'seria-toggler';
-        control.innerHTML = `<input type="checkbox" checked name="${config.name}"/><span class="seria-toggler-checkmark" style="color: ${config.color}"></span>${config.title}`;
-        control.onchange = this.handleOnToggle(config.name);
-        return control;
+        let control = document.createElement('label')
+        control.className = 'seria-toggler'
+        control.innerHTML = `<input type="checkbox" checked name="${config.name}"/><span class="seria-toggler-checkmark" style="color: ${config.color}"></span><span>${config.title}</span>`
+        control.onchange = this.handleOnToggle(config.name)
+        this.activeCount++
+        return control
     }
 }
